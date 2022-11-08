ff89a6cafef1e72ec4e27a8d1adafe56ee294bb9
Sam Adams <sam.adams7785@gmail.com>
2022-11-08 10:37:10 -0800

    initial and books test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,9 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+const { books } = require('../lib/books-data');
 // const request = require('supertest');
 // const app = require('../lib/app');
 
@@ -7,9 +11,13 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('/books should return a list of books', async () => {
+    const res = await request(app).get('/books');
+    const expected = books.map(({ id, name }) => ({ id, name }));
+    expect(res.body).toEqual(expected);
   });
+
   afterAll(() => {
     pool.end();
   });
